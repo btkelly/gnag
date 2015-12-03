79ad905f41382596093432e2332284d1e8afc73f
Bryan Kelly <bkelly@detroitlabs.com>
2015-12-03 13:45:00 -0500

    Updated logging interceptor for body and debug modes

--- src/main/java/com/btkelly/gnag/api/LoggingInterceptor.java
+++ src/main/java/com/btkelly/gnag/api/LoggingInterceptor.java
@@ -4,8 +4,12 @@ import com.btkelly.gnag.utils.Logger;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.logging.HttpLoggingInterceptor;
+import com.squareup.okhttp.logging.HttpLoggingInterceptor.Level;
 
 import java.io.IOException;
+
+import static com.squareup.okhttp.logging.HttpLoggingInterceptor.Level.BODY;
+import static com.squareup.okhttp.logging.HttpLoggingInterceptor.Level.NONE;
 
 /**
  * Created by bobbake4 on 12/3/15.
@@ -15,15 +19,21 @@ public class LoggingInterceptor implements Interceptor {
     private final HttpLoggingInterceptor httpLoggingInterceptor;
 
     public LoggingInterceptor() {
+
+        Level level;
+
+        if (Logger.isDebugLogEnabled()) {
+            level = BODY;
+        } else {
+            level = NONE;
+        }
+
         this.httpLoggingInterceptor = new HttpLoggingInterceptor();
+        this.httpLoggingInterceptor.setLevel(level);
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        if (Logger.isDebugLogEnabled()) {
-            return httpLoggingInterceptor.intercept(chain);
-        } else {
-            return chain.proceed(chain.request());
-        }
+        return httpLoggingInterceptor.intercept(chain);
     }
 }
