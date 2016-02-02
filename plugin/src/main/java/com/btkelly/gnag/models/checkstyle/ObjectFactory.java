5a7b5ab246ae11f1f32002f22c1d9c1f5bed7da8
Bryan Kelly <bkelly@detroitlabs.com>
2016-02-02 16:09:20 -0500

    Updated license year

--- plugin/src/main/java/com/btkelly/gnag/models/checkstyle/ObjectFactory.java
+++ plugin/src/main/java/com/btkelly/gnag/models/checkstyle/ObjectFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Bryan Kelly
+ * Copyright 2016 Bryan Kelly
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 package com.btkelly.gnag.models.checkstyle;
 
 import javax.xml.bind.JAXBElement;
