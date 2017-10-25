3a1e19d85ac4a00e3b51846054458ce58b80e924
Stuart Kent <skent.dev@gmail.com>
2017-10-25 14:29:30 +0100

    Add Kotlin example project

---
+++ example-kotlin/src/main/kotlin/com/btkelly/gnag/example/Main.kt
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016 Bryan Kelly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.btkelly.gnag.example
+
+/**
+ * Created by bobbake4 on 2/7/17.
+ */
+class Main {
+
+    override fun toString(): String = super.toString()
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            val test = ""
+            val testCompare = "Fail"
+
+            if (test === testCompare) {
+                print("Total Fail")
+            }
+
+            try {
+                print("Empty")
+            } catch (e: Exception) {
+
+            } finally {
+
+            }
+        }
+    }
+
+}
